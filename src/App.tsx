9ea12056a74ee024c8e7367118f130159babec47
Ronye Vernaes <remnahush@gmail.com>
2025-07-27 10:31:13 +0200

    initial app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Data Viz Pipelines</h1>
+      <div className="">
+      </div>
+    </>
+  )
+}
+
+export default App
