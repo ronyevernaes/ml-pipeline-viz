2538c636959be0782154998bee5f6c2f3caba79d
Ronye Vernaes <remnahush@gmail.com>
2025-07-27 12:20:09 +0200

    add base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold">Data Viz Pipelines</h1>
-      <div className="">
-      </div>
-    </>
-  )
-}
+    <div className="flex flex-col min-h-screen">
+      <header className="p-4 border-b border-stone-700">
+        <h1 className="text-3xl font-bold">Data Viz Pipelines</h1>
+      </header>
 
-export default App
+      <main className="flex-1 p-4 flex">
+        Content here...
+      </main>
+
+      <footer className="p-2 border-t border-stone-700 text-center text-sm text-stone-600">
+        ZenML Pipeline Visualization
+      </footer>
+    </div>
+  )
+};
+
+export default App;
